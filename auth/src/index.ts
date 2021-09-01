d157c14ede1e2890af3e4929e2f6b4339d431b74
Pedro <phd208@students.cs.ubc.ca>
2021-08-31 18:32:43 -0700

    auth startup message update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 
 const start = async () => {
 
-  console.log('Starting AUTH now...');
+  console.log('Starting AUTH RIGHT NOW...');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
